bd59cf218b7f703acc8d5cd2160344a7705ba66e
ErickJmzAnaya <129359869+ErickJmzAnaya@users.noreply.github.com>
2024-06-23 05:22:26 -0600

    checkbox en filtrarRecetas.html

--- logicaNegocio/static/logicaNegocio/js/recetas.js
+++ logicaNegocio/static/logicaNegocio/js/recetas.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
     const forms = document.querySelectorAll(".fav-form");
     forms.forEach(form => {
         const checkbox = form.querySelector(".favorito-checkbox");
@@ -6,19 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
             const recetaId = form.getAttribute("data-receta-id");
             const recetaTitulo = form.getAttribute("data-receta-titulo");
             const recetaImagen = form.getAttribute("data-receta-imagen");
+            const recetaUrl = recetaId; // Asumimos que recetaId es la URL de la receta
             const isChecked = checkbox.checked;
 
-            fetch("{% url 'recetas:ToggleFavorito' %}", {
+            fetch("/toggle_favorito/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "X-CSRFToken": "{{ csrf_token }}"
+                    "X-CSRFToken": csrfToken
                 },
                 body: JSON.stringify({
                     receta_id: recetaId,
                     receta_titulo: recetaTitulo,
                     receta_imagen: recetaImagen,
-                    receta_url: recetaId,  // Incluir la URL de la receta
+                    receta_url: recetaUrl,
                     is_checked: isChecked
                 })
             })
